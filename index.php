0f58f39e6ece42b2ae958610703fd51e643a3730
giter <nubix@qq.com>
2012-08-18 22:47:23 +0800

    Init commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html >
+<html>
+	<head>
+		<link rel='stylesheet' href='./css/bootstrap.css'/>
+		<link rel='stylesheet' href='./css/global.css'/>
+	</head>
+	<body id='page-index'>
+
+		<div class='navbar navbar-fixed-top'>
+			<div class='navbar-inner'>
+				<div class='container'>
+					<div class='brand'>LEE</div>
+					<ul class='nav'>
+						<li class='active'><a  href='#'>HOME</a></li>
+						<li><a href='#'>BLOG</a></li>
+						<li><a href='#'>WIKI</a></li>
+						<li><a href='#'>UTILS</a></li>
+						<li><a href='#'>CONTACTS</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+
+		<div class='container'>
+			<div class='hero-unit'>
+				<h2>Try to make my world simple and stupid ...</h2>
+			</div>
+			<div id='functions' class='row-fluid'>
+				<div class='span4 hero-unit' style='padding:18px'>
+					<h3>Blogs</h3>
+					<p class='alert alert-info'>
+						<strong class='label label-info'>BLOG</strong> records my workings.
+					</p>
+					<a href='#' class='pull-right btn'>take a look</a>
+				</div>
+				<div class='span4 hero-unit' style='padding:18px'>
+					<h3>Wikis</h3>
+					<p class='alert alert-info'>
+						<strong class='label label-info'>WIKI</strong> contains my knowledge.
+					</p>
+					<a href='#' class='pull-right btn'>take a look</a>
+				</div>
+				<div class='span4 hero-unit' style='padding:18px'>
+					<h3>Utils</h3>
+					<p class='alert alert-info'>
+						<strong class='label label-info'>UTIL</strong> is cool.
+					</p>
+					<a href='#' class='pull-right btn'>take a look</a>
+				</div>
+			</div>
+			<div class='footer'>
+				<p class='pull-right'>
+					<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.zh">
+						<img style="border-width:0" src="./img/license.png" />
+					</a>
+					&nbsp;&nbsp;by <strong>LEE</strong>
+				</p>
+			</div>
+		</div>
+		
+	</body>
+</html>
