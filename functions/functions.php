cf25d9f3a1c018cb7d761063d289160385ebee6f
nubix <nubix@qq.com>
2012-08-19 18:44:42 +0800

    add get() function to get object from db

--- functions/functions.php
+++ functions/functions.php
@@ -5,27 +5,29 @@ function l($url){
 }
 
 function db(){
-	
+
 	global $db;
-	
+
 	if(!isset($db)){
 		$m = new Mongo("127.0.0.1");
-		
+
 		$db = $m->lee;
 
 		if(isset($DB["user"]) && isset($DB["password"])){
 			$db->authenticate($DB["user"],$DB["password"]);
 		}
 	}
-	
+
 	return $db;
 }
 
-function obj($type,$id,$fields){
+function get($type,$id,$fields=NULL){
+	$c = db()->$type;
+	return $fields ? $c->find(array("_id"=>$id),$fields) : $c->find(array("_id"=>$id));
 }
 
 
 function view_blogs(){
-	$_REQUEST['blogs'] = db()->blog->find()->limit(10)->sort(array("_id"=>-1));
+	$_REQUEST['blogs'] = db()->blog->find()->limit(10)->sort(array("_id" => -1));
 }
 
