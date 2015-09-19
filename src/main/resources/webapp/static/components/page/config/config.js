362e887dbe549150a5fa215a1c43d3ae365e79ac
李江峰 <hao3304@qq.com>
2015-09-20 01:27:32 +0800

    最新更新

--- src/main/resources/webapp/static/components/page/config/config.js
+++ src/main/resources/webapp/static/components/page/config/config.js
@@ -7,12 +7,40 @@ define('components/page/config/config', function(require, exports, module) {
 var Vue = require("component_modules/vue");
 var nav = require("navbar/navbar.js");
 var Service = require("main/service.js");
-
+var Layer = require("component_modules/layer.m");
 module.exports =   Vue.extend({
-   template:"<div class=\"page-config\">\r\n    <div class=\"mui-control-content mui-active\">\r\n        <header class=\"mui-bar-nav mui-bar\">\r\n            <h5 class=\"mui-title\">\r\n                订单查询\r\n            </h5>\r\n        </header>\r\n        <div class=\"mui-content\">\r\n            <ul class=\"mui-table-view mui-list\">\r\n                <i class=\" mui-pull-left iconfont icon-user\"></i>\r\n                <li class=\"mui-table-view-cell\">\r\n                    <a class=\"mui-navigate-right\" href=\"#bind\" >\r\n                        绑定用户\r\n                    </a>\r\n                </li>\r\n            </ul>\r\n\r\n            <ul class=\"mui-table-view mui-list\" style=\"margin-top: 25px;\">\r\n                <i class=\" mui-pull-left iconfont icon-users\"></i>\r\n                <li class=\"mui-table-view-cell\">\r\n                    <a class=\"mui-navigate-right\" href=\"#relation\">\r\n                        关联用户\r\n                    </a>\r\n                </li>\r\n            </ul>\r\n        </div>\r\n\r\n\r\n    </div>\r\n   <c-nav view=\"config\"></c-nav>\r\n</div>",
+   template:"<div class=\"page-config\">\r\n    <div class=\"mui-control-content mui-active\">\r\n        <header class=\"mui-bar-nav mui-bar\">\r\n            <h5 class=\"mui-title\">\r\n                订单查询\r\n            </h5>\r\n        </header>\r\n        <div class=\"mui-content\">\r\n            <ul class=\"mui-table-view mui-list\">\r\n                <i class=\" mui-pull-left iconfont icon-user\"></i>\r\n                <li class=\"mui-table-view-cell\">\r\n                    <a >\r\n                        {{mine.email}}111\r\n                        <button v-on=\"tap:unBind\" class=\"mui-btn mui-btn-danger\">解绑</button>\r\n                    </a>\r\n                </li>\r\n            </ul>\r\n\r\n            <ul class=\"mui-table-view mui-list\" style=\"margin-top: 25px;\">\r\n                <i class=\" mui-pull-left iconfont icon-users\"></i>\r\n                <li class=\"mui-table-view-cell\">\r\n                    <a class=\"mui-navigate-right\" href=\"#relation\">\r\n                        关联用户\r\n                    </a>\r\n                </li>\r\n            </ul>\r\n        </div>\r\n\r\n\r\n    </div>\r\n   <c-nav view=\"config\"></c-nav>\r\n</div>",
+   data: function () {
+      return {
+         mine:{}
+      }
+   },
+   methods:{
+      unBind: function () {
+         Layer.open({
+            content:"确定解绑该账号？",
+            btn:["确定","取消"],
+            yes: function () {
+               Layer.closeAll();
+            },
+            no: function () {
+               Layer.closeAll();
+            }
+         });
+      } 
+   },
    ready: function () {
+      var self = this;
+      Layer.open({
+         content:"加载中",
+         type:2,
+         shadeClose:false
+      });
       Service.getMine(function (rep) {
-         alert(JSON.stringify(rep));
+         Layer.closeAll();
+         if(rep.Code == 0){
+            self.mine = rep.Response;
+         }
       })
    }
 });
