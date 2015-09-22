b74b91f0bda5201771db55f516011f034d20cad4
李江峰 <hao3304@qq.com>
2015-09-22 09:47:42 +0800

    最新更新

--- src/main/resources/webapp/static/components/page/relation/query/query.js
+++ src/main/resources/webapp/static/components/page/relation/query/query.js
@@ -9,7 +9,7 @@ var Layer = require("component_modules/layer.m").layer;
 var Service = require("main/service.js");
 
 module.exports =   Vue.extend({
-    template:"<div class=\"page-query\"  >\r\n    <header class=\"mui-bar mui-bar-nav\">\r\n        <div class=\"mui-input-row mui-search mui-pull-left\" style=\"max-width: 80%\">\r\n            <input type=\"search\" class=\"mui-input-clear\" placeholder=\"请输入姓名查询\" v-model=\"name\" >\r\n        </div>\r\n        <a class=\"mui-pull-right mui-btn-link\" href=\"#relation\">取消</a>\r\n    </header>\r\n    <div class=\"mui-content\" v-if=\"list.length>0\">\r\n        <ul class=\"mui-table-view  mui-media\">\r\n            <li class=\"mui-table-view-cell mui-media-body\" v-repeat=\"u in list\">\r\n                {{u.name}}\r\n                <p>{{u.email}}</p>\r\n                <button class=\"mui-btn mui-btn-outlined mui-btn-negative\" v-on=\"click:addDelegation(u._id)\">委托</button>\r\n            </li>\r\n        </ul>\r\n    </div>\r\n\r\n</div>",
+    template:"<div class=\"page-query\"  >\r\n    <header class=\"mui-bar mui-bar-nav\">\r\n        <div class=\"mui-input-row mui-search mui-pull-left\" style=\"width: 80%\">\r\n            <input type=\"search\" class=\"mui-input-clear\" placeholder=\"请输入姓名查询\" v-model=\"name\" >\r\n        </div>\r\n        <a class=\"mui-pull-right mui-btn-link\" href=\"#relation\">取消</a>\r\n    </header>\r\n    <div class=\"mui-content\" v-if=\"list.length>0\">\r\n        <ul class=\"mui-table-view  mui-media\">\r\n            <li class=\"mui-table-view-cell mui-media-body\" v-repeat=\"u in list\">\r\n                {{u.name}}\r\n                <p>{{u.email}}</p>\r\n                <button class=\"mui-btn mui-btn-outlined mui-btn-negative\" v-on=\"click:addDelegation(u._id)\">委托</button>\r\n            </li>\r\n        </ul>\r\n    </div>\r\n\r\n</div>",
     data: function () {
         return {
             list:[],
@@ -80,7 +80,7 @@ module.exports =   Vue.extend({
         }
     },
     ready: function () {
-        mui(".page-query input").input();
+        //mui(".page-query input").input();
     }
 });
 
