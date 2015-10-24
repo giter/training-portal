9b82913844c90070e203b8e19610e0945bb4815b
giter <giter@users.noreply.github.com>
2015-10-24 21:55:16 +0800

    commit 20151024.01

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+ps aux | grep busorder | awk '{print $2}' | xargs kill -9
+rm target/busorder-*-SNAPSHOT.jar -fv
+
+pushd training-portal.git/branches/game/
+
+
+svn update
+mvn package
+
+cp target/busorder-*-SNAPSHOT.jar ~
+
+popd
+
+nohup java -jar -Dport=80 busorder-*-SNAPSHOT.jar &
+
