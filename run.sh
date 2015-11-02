097c05856beae6ec02e221d0dc9277cc3e651410
giter <giter@users.noreply.github.com>
2015-11-02 23:08:30 +0800

    commit 20151102.01

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+DIR=`pwd`
 
 ps aux | grep busorder- | awk '{print $2}' | xargs kill -9
 rm target/busorder-*-SNAPSHOT.jar -fv
-
 pushd training-portal.git/branches/game/
 
 
 svn update
-mvn clean package
+mvn --quiet -DskipTests=true clean package
 
-cp target/busorder-*-SNAPSHOT.jar ~
+cp target/busorder-*-SNAPSHOT.jar ${DIR}
 
 popd
 
