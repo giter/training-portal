0f9e085eac08d4f3a3e9e295718555ee285ce8c8
jack <hao3304@qq.com>
2016-05-12 11:47:29 +0800

    commit 2016-5-12

--- src/main/resources/webapp/order/admin/static/js/jquery.jqprint-0.3.js
+++ src/main/resources/webapp/order/admin/static/js/jquery.jqprint-0.3.js
@@ -1,4 +1,4 @@
-// -----------------------------------------------------------------------
+﻿// -----------------------------------------------------------------------
 // Eros Fratini - eros@recoding.it
 // jqprint 0.3
 //
@@ -52,19 +52,20 @@
                 });
             }
         }
+
+        doc.write("<style>@media print {.table{border-spacing: 0; border-collapse: collapse;}.table td{border:1px solid #ddd;padding: 5px;font-size: 14px;}}</style>");
         
         if (opt.printContainer) { doc.write($element.outer()); }
         else { $element.each( function() { doc.write($(this).html()); }); }
         
         doc.close();
-        
         (opt.operaSupport && $.browser.opera ? tab : $iframe[0].contentWindow).focus();
         setTimeout( function() { (opt.operaSupport && $.browser.opera ? tab : $iframe[0].contentWindow).print(); if (tab) { tab.close(); } }, 1000);
     }
     
     $.fn.jqprint.defaults = {
-		debug: false,
-		importCSS: true, 
+		debug: true,
+		importCSS: false,
 		printContainer: true,
 		operaSupport: true
 	};
