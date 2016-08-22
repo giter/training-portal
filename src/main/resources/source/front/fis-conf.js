c9cf4c840d0f056c2aa6330bb5e2c2f7a4afd887
jack <hao3304@qq.com>
2016-08-22 23:21:37 +0800

    commit 2016-8-22

--- src/main/resources/source/front/fis-conf.js
+++ src/main/resources/source/front/fis-conf.js
@@ -1,19 +1,20 @@
+var path = "";
+//var path = "";
+
 
 fis.hook('module', {
     mode: 'mod'
-    /*paths : {
-     'main': 'components/component/main'
-     }*/
 });
 
-
-//components下面的所有js资源都是组件化资源
 fis.match("components/**", {
     isMod: true,
-    release: '/static/$0'
+    release: path+'/static/$0'
 });
 
-//doc目录不发布
+fis.match("data/**", {
+    release: path+'/$0'
+});
+
 fis.match("doc/**", {
     release: false
 });
@@ -21,59 +22,64 @@ fis.match("doc/**", {
 fis.match("/component_modules/*.js", {
     isMod: true,
     useMap: true,
-    release: '/static/$0'
+    release: path+'/static/$0'
 });
 
-//component组件资源id支持简写
 fis.match(/^\/components\/component\/(.*)$/i, {
-    id : '$1'
+    id : '$1',
+    useHash:false
 });
 
 
-//page里的页面发布到根目录
 fis.match("components/page/(*.html)",{
-    release: '/$1',
+    release: path+'/$1',
     useCache : false
 });
 
-//sass的编译
-fis.match('**/*.less', {
-    rExt: '.css', // from .scss to .css
-    parser: fis.plugin('less', {
-        //fis-parser-sass option
-    })
-});
+//fis.match('**/*.less', {
+//    rExt: '.css', // from .scss to .css
+//    parser: fis.plugin('less2', {
+//        //fis-parser-sass option
+//    })
+//});
 
-//文章封面和作者头像等动态图片地址不加hash
 fis.match(/static\/images\/.*\.(jpeg|jpg|png)$/,{
-    useHash: false
-})
+    useHash: false,
+    release: path+'/$0'
+});
 
 fis.match('::packager', {
     // npm install [-g] fis3-postpackager-loader
-    // 分析 __RESOURCE_MAP__ 结构，来解决资源加载问题
     postpackager: fis.plugin('loader', {
         resourceType: 'mod',
-        useInlineMap: true // 资源映射表内嵌
+        useInlineMap: true
     }),
     packager: fis.plugin('map'),
     spriter: fis.plugin('csssprites', {
         layout: 'matrix',
         margin: '15'
     })
+}).match('**/*.css', {
+    packTo: '/static/pkg/all.css'//css鎵撴垚涓�涓寘
+});
 
-}).match('**/*.{css,scss}', {
-    packTo: '/static/pkg/all.css' //css打成一个包
-})
+fis.match("/static/**/*.*",{
+    useHash:false,
+    release:path+"/$0"
+});
 
-//生产环境下CSS、JS压缩合并
-//使用方法 fis3 release prod
+//fis.match("*.{js,css,png}",{
+//    useHash:true
+//})
 fis.media('prod')
     .match('component_modules/*.js',{
         packTo: '/static/pkg/common.js'
     })
     .match('components/**/*.js',{
         packTo: '/static/pkg/app.js'
+    })
+    .match('/static/pkg/*.css', {
+        optimizer: fis.plugin('clean-css')
     })
     .match('/static/pkg/**',{
         useHash:true
