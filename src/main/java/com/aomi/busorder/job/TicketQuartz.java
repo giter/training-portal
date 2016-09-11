41b06f3f30f3979b7434c81e13792466aa65ab97
Lijiajie <lijiajie@chinahddz.com>
2016-09-11 15:56:57 +0800

    add default user.

--- src/main/java/com/aomi/busorder/job/TicketQuartz.java
+++ src/main/java/com/aomi/busorder/job/TicketQuartz.java
@@ -15,8 +15,8 @@ import org.springframework.stereotype.Component;
 
 import com.aomi.busorder.Ldap.DUser;
 import com.aomi.busorder.Ldap.Department;
-import com.aomi.busorder.Ldap.Usera;
 import com.aomi.busorder.Ldap.LdapService;
+import com.aomi.busorder.Ldap.Usera;
 import com.aomi.busorder.pojo.User;
 import com.aomi.busorder.service.TicketService;
 import com.aomi.busorder.service.UserService;
@@ -37,71 +37,76 @@ public class TicketQuartz {
   @Scheduled(cron = "30 14 13 * * ?")
   public void generate() {
 
-	  SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
-	    Calendar cal = Calendar.getInstance();
+    Calendar cal = Calendar.getInstance();
 
-	    String date = sdf.format(cal.getTime());
+    String date = sdf.format(cal.getTime());
 
-	    LOGGER.info(String.format("Clean old ticket %s", date));
-	    ticketService.clean(date);
+    LOGGER.info(String.format("Clean old ticket %s", date));
+    ticketService.clean(date);
 
-	    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) {
 
-	      cal.add(Calendar.DATE, 1);
+      cal.add(Calendar.DATE, 1);
 
-	      int num = ticketService.generateAll(sdf.format(cal.getTime()));
-	      LOGGER.info(String.format("Generate %d ticket on %s", num,
-	          sdf.format(cal.getTime())));
+      int num = ticketService.generateAll(sdf.format(cal.getTime()));
+      LOGGER.info(String.format("Generate %d ticket on %s", num,
+          sdf.format(cal.getTime())));
 
-	    }
+    }
 
-   
-	  LdapService a=new LdapService();
-	  List<Usera>users=a.getUserInfo();
-		List<DUser>dusers=a.getDeptInfoa(0);
-		
-		List<Department> departmentList=a.getDeptInfo(0);
-		
-		List<Department> departmentList1=a.getDeptInfo(1, departmentList);
-		List<User> list=new ArrayList<User>();
-		
-		for(int i=0;i<users.size();i++){
-			users.get(i).getEmail();
-			User user=new User();
-			user.setEmail(users.get(i).getEmail());
-			List<User>ulist=userService.findusers(user);
-			if(ulist.size()==0){
-				user.setPassword("qwer");
-				user.setName(users.get(i).getUserName());
-				for(int j=0;j<dusers.size();j++){
-					if(dusers.get(j).getUID().equals(users.get(i).getUid())){
-						for(int k=0;k<departmentList1.size();k++){
-							if(dusers.get(j).getDID().equals(departmentList1.get(k).getDID())){
-								user.setUnit(departmentList1.get(k).getDepartmentName());
-								if(departmentList1.get(k).getLevel()==3){
-									for(int g=0;g<departmentList1.size();g++){
-										if(departmentList1.get(g).getDID().equals(departmentList1.get(k).getParentID())){
-											user.setDepartment(departmentList1.get(g).getDepartmentName());
-										}
-									}
-								}
-							}
-						}
-						
-					}
-				}
-				list.add(user);
-				if(user!=null){
-				if(user.getUnit()!=null&&!user.getUnit().equals("")){
-				userService.insert(user);
-				}
-				}
-			}
-		}
-		System.out.println("Number: " + list.size());
+    LdapService a = new LdapService();
+    List<Usera> users = a.getUserInfo();
+    List<DUser> dusers = a.getDeptInfoa(0);
+
+    List<Department> departmentList = a.getDeptInfo(0);
+
+    List<Department> departmentList1 = a.getDeptInfo(1, departmentList);
+    List<User> list = new ArrayList<User>();
+
+    for (int i = 0; i < users.size(); i++) {
+      users.get(i).getEmail();
+      User user = new User();
+      user.setEmail(users.get(i).getEmail());
+      List<User> ulist = userService.findusers(user);
+      if (ulist.size() == 0) {
+        user.setPassword("qwer");
+        user.setName(users.get(i).getUserName());
+        for (int j = 0; j < dusers.size(); j++) {
+          if (dusers.get(j).getUID().equals(users.get(i).getUid())) {
+            for (int k = 0; k < departmentList1.size(); k++) {
+              if (dusers.get(j).getDID()
+                  .equals(departmentList1.get(k).getDID())) {
+                user.setUnit(departmentList1.get(k).getDepartmentName());
+                if (departmentList1.get(k).getLevel() == 3) {
+                  for (int g = 0; g < departmentList1.size(); g++) {
+                    if (departmentList1.get(g).getDID()
+                        .equals(departmentList1.get(k).getParentID())) {
+                      user.setDepartment(
+                          departmentList1.get(g).getDepartmentName());
+                    }
+                  }
+                }
+              }
+            }
+
+          }
+        }
+        list.add(user);
+        if (user != null) {
+          if (user.getUnit() != null && !user.getUnit().equals("")) {
+
+            user.setType(0);
+            user.setAdmin(0);
+            user.setLimit(1);
+
+            userService.insert(user);
+          }
+        }
+      }
+    }
+    System.out.println("Number: " + list.size());
   }
-  
- 
-  
+
 }
